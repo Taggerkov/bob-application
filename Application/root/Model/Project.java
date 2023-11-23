aca0dd156e45905d87895b033ed82e34be623d66
Taggerkov <Taggerkov@protonmail.ch>
2023-11-23 15:06:40 +0100

    Project DONE
    Check the String to Int if you want.
    Do we add the try-catch here or in controller?

--- Application/root/Model/Project.java
+++ Application/root/Model/Project.java
@@ -1,4 +1,52 @@
 package Model;
 
 public abstract class Project {
+    private double budget;
+    private Resources resources;
+    private MyDate startDate;
+    private MyDate endDate;
+    private boolean isActive;
+
+    public Project(double budget, String start, String end) {
+        this.budget = budget;
+        this.resources = new Resources();
+
+        String[] temp = start.split("\\.");
+        int tempDay = Integer.parseInt(temp[0]);
+        int tempMonth = Integer.parseInt(temp[1]);
+        int tempYear = Integer.parseInt(temp[2]);
+        this.startDate = new MyDate(tempDay,tempMonth,tempYear);
+
+        temp = end.split("\\.");
+        tempDay = Integer.parseInt(temp[0]);
+        tempMonth = Integer.parseInt(temp[1]);
+        tempYear = Integer.parseInt(temp[2]);
+        this.endDate = new MyDate(tempDay,tempMonth,tempYear);
+
+        this.isActive = true;
+    }
+    
+    public Project(MyDate startDate) {
+        this.budget = 0;
+        this.resources = new Resources();
+        this.startDate = startDate;
+        this.endDate = null;
+        this.isActive = true;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
 }
