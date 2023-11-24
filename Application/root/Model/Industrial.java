af39c3abd43d226faa263c63c8d471c0838f2576
Bibekk49 <bibekpaudel793@gmail.com>
2023-11-24 19:46:03 +0100

    .

--- Application/root/Model/Industrial.java
+++ Application/root/Model/Industrial.java
@@ -2,4 +2,32 @@ package Model;
 
 public class Industrial
 {
+  private double size;
+  private String typeOfFacility;
+
+  public Industrial(double size, String typeOfFacility)
+  {
+    this.size = size;
+    this.typeOfFacility = typeOfFacility;
+  }
+
+  public double getSize()
+  {
+    return size;
+  }
+
+  public void setSize(double size)
+  {
+    this.size = size;
+  }
+
+  public String getTypeOfFacility()
+  {
+    return typeOfFacility;
+  }
+
+  public void setTypeOfFacility(String typeOfFacility)
+  {
+    this.typeOfFacility = typeOfFacility;
+  }
 }
