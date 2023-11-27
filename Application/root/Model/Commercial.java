6028cc710319aa51f3c0546800c6575ab62ba456
BattyAlex <Alexandriaahansen@gmail.com>
2023-11-27 12:36:14 +0100

    Initialising fields

--- Application/root/Model/Commercial.java
+++ Application/root/Model/Commercial.java
@@ -1,44 +1,55 @@
 package Model;
 
-public class Commercial extends Project {
+public class Commercial extends Project
+{
     private int noOfFloors;
     private double size;
     private String intendedUse;
 
-    public Commercial(double budget, String start, String end, int noOfFloors, double size, String intendedUse) {
+    public Commercial(double budget, String start, String end, int noOfFloors, double size, String intendedUse)
+    {
         super(budget, start, end);
         this.noOfFloors = noOfFloors;
         this.size = size;
         this.intendedUse = intendedUse;
     }
 
-    public Commercial(){
+    public Commercial()
+    {
         super(500000, String.valueOf(java.time.LocalDate.now()));
         this.noOfFloors = 1;
         //end 'Date' would default 18 months.
+        size = 0;
+        intendedUse = "Unspecified";
     }
 
-    public int getNoOfFloors() {
+    public int getNoOfFloors()
+    {
         return noOfFloors;
     }
 
-    public void setNoOfFloors(int noOfFloors) {
+    public void setNoOfFloors(int noOfFloors)
+    {
         this.noOfFloors = noOfFloors;
     }
 
-    public double getSize() {
+    public double getSize()
+    {
         return size;
     }
 
-    public void setSize(double size) {
+    public void setSize(double size)
+    {
         this.size = size;
     }
 
-    public String getIntendedUse() {
+    public String getIntendedUse()
+    {
         return intendedUse;
     }
 
-    public void setIntendedUse(String intendedUse) {
+    public void setIntendedUse(String intendedUse)
+    {
         this.intendedUse = intendedUse;
     }
 }
