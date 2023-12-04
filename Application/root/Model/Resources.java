b543e0407fa70e7cbdc3b2c63e12c23345a044ec
razzban <149073260+razzban@users.noreply.github.com>
2023-12-04 12:31:25 +0100

    java docs

--- Application/root/Model/Resources.java
+++ Application/root/Model/Resources.java
@@ -1,70 +1,126 @@
 package Model;
 
-public class Resources
-{
+/**
+ * The {@code Resources} class represents resources associated with a project.
+ * It includes information about man-hours, expenses, and expected values for both.
+ *
+ * @author Raj Ban
+ * @version 1.0
+ */
+public class Resources {
   private double manHours;
   private double expenses;
   private double expectedManHours;
   private double expectedExpenses;
 
-  public Resources(double expectedExpenses, double expectedManHours)
-  {
+  /**
+   * Constructs a new Resources object with the specified expected expenses and man-hours.
+   *
+   * @param expectedExpenses The expected expenses for the project.
+   * @param expectedManHours The expected man-hours for the project.
+   */
+  public Resources(double expectedExpenses, double expectedManHours) {
     this.expectedExpenses = expectedExpenses;
     this.expectedManHours = expectedManHours;
     expenses = 0;
     manHours = 0;
   }
-  public Resources()
-{
-  manHours = 0;
-  expenses = 0;
-  expectedExpenses = 0;
-  expectedManHours = 0;
-}
 
-public void setManHours(double manHours)
-{
-  this.manHours=manHours;
-}
+  /**
+   * Constructs a new Resources object with default values for expenses and man-hours.
+   */
+  public Resources() {
+    manHours = 0;
+    expenses = 0;
+    expectedExpenses = 0;
+    expectedManHours = 0;
+  }
 
-public double getManHours()
-{
-  return manHours;
-}
-public void setExpenses()
-{
-  this.expenses=expenses;
-}
-  public double getExpenses()
-  {
+  /**
+   * Sets the actual man-hours for the project.
+   *
+   * @param manHours The actual man-hours spent on the project.
+   */
+  public void setManHours(double manHours) {
+    this.manHours = manHours;
+  }
+
+  /**
+   * Gets the actual man-hours for the project.
+   *
+   * @return The actual man-hours spent on the project.
+   */
+  public double getManHours() {
+    return manHours;
+  }
+
+  /**
+   * Sets the actual expenses for the project.
+   *
+   * @param expenses The actual expenses incurred for the project.
+   */
+  public void setExpenses(double expenses) {
+    this.expenses = expenses;
+  }
+
+  /**
+   * Gets the actual expenses for the project.
+   *
+   * @return The actual expenses incurred for the project.
+   */
+  public double getExpenses() {
     return expenses;
   }
 
-  public double getExpectedManHours()
-  {
+  /**
+   * Gets the expected man-hours for the project.
+   *
+   * @return The expected man-hours for the project.
+   */
+  public double getExpectedManHours() {
     return expectedManHours;
   }
 
-  public void setExpectedManHours(double expectedManHours)
-  {
+  /**
+   * Sets the expected man-hours for the project.
+   *
+   * @param expectedManHours The expected man-hours for the project.
+   */
+  public void setExpectedManHours(double expectedManHours) {
     this.expectedManHours = expectedManHours;
   }
 
-  public double getExpectedExpenses()
-  {
+  /**
+   * Gets the expected expenses for the project.
+   *
+   * @return The expected expenses for the project.
+   */
+  public double getExpectedExpenses() {
     return expectedExpenses;
   }
 
-  public void setExpectedExpenses(double expectedExpenses)
-  {
+  /**
+   * Sets the expected expenses for the project.
+   *
+   * @param expectedExpenses The expected expenses for the project.
+   */
+  public void setExpectedExpenses(double expectedExpenses) {
     this.expectedExpenses = expectedExpenses;
   }
-  public boolean equals(Object obj)
-  {
-    if (obj== null || obj.getClass()!=this.getClass())
+
+  /**
+   * Checks if this Resources object is equal to another object.
+   *
+   * @param obj The object to compare for equality.
+   * @return {@code true} if the objects are equal, {@code false} otherwise.
+   */
+
+
+  public boolean equals(Object obj) {
+    if (obj == null || obj.getClass() != this.getClass())
       return false;
-    Resources other=(Resources) obj;
-    return manHours==other.manHours && expenses==other.expenses && expectedManHours== other.expectedManHours && expectedExpenses==other.expectedExpenses ;
+    Resources other = (Resources) obj;
+    return manHours == other.manHours && expenses == other.expenses &&
+        expectedManHours == other.expectedManHours && expectedExpenses == other.expectedExpenses;
   }
 }
-
