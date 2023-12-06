8078ea40d245322a01a2607787b35f4a12f763d7
BattyAlex <Alexandriaahansen@gmail.com>
2023-12-06 12:08:15 +0100

    Javadocs

--- Application/root/Model/ProjectList.java
+++ Application/root/Model/ProjectList.java
@@ -2,14 +2,28 @@ package Model;
 
 import java.util.ArrayList;
 
+/**
+ * Class containing a list of projects
+ * @author Alexandria Hansen
+ * @version 1.0
+ */
 public class ProjectList
 {
   private ArrayList<Project> projects;
-  private ArrayList<String> titles;
 
+  /**
+   * No-argument constructor initialising the ProjectList
+   */
   public ProjectList()
   {
+    projects = new ArrayList<Project>();
   }
+
+  /**
+   * Get method for a student at a given index
+   * @param index position in the list
+   * @return Project at the given position in the list
+   */
   public Project get(int index)
   {
     if(index<projects.size())
@@ -21,10 +35,21 @@ public class ProjectList
       return null;
     }
   }
+
+  /**
+   * Get method for the amount of projects in the list
+   * @return number of projects in the list
+   */
   public int size()
   {
     return projects.size();
   }
+
+  /**
+   * Get method for the type of the project located at the given position in the list
+   * @param index position of the project in the lis
+   * @return String with the type of the Project at the given index
+   */
   public String getType(int index)
   {
     if (projects.get(index) instanceof Residential )
@@ -38,11 +63,21 @@ public class ProjectList
     else
       return "OTHER";
   }
+
+  /**
+   * Adds a project to the list
+   * @param project the project to be added to the list
+   */
   public void add(Project project)
   {
     projects.add(project);
   }
 
+  /**
+   * Get method for the title of a project on a given position in the list
+   * @param index the position of the project in the list
+   * @return the title of the project at the given index
+   */
   public String getTitle(int index)
   {
     return projects.get(index).getTitle();
