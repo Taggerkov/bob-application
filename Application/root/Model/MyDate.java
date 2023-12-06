df9f482d054edf1253e4597dbb728469e10fd52b
SalomeeaTricolici <149162841+SalomeeaTricolici@users.noreply.github.com>
2023-12-06 12:15:13 +0100

    MyDate java docs

--- Application/root/Model/MyDate.java
+++ Application/root/Model/MyDate.java
@@ -1,15 +1,32 @@
 package Model;
 
 public class MyDate {
+    /**
+     * class for MyDate
+     * @author Sergiu Chirap
+     * @version 1.0
+     */
     private int day;
     private int month;
     private int year;
+
+    /**
+     * Full constructor for MyDate class
+     * @param day
+     * @param month
+     * @param year
+     */
 
     public MyDate(int day, int month, int year) {
         this.day = day;
         this.month = month;
         this.year = year;
     }
+
+    /**constructor for MyDate class, default values
+     *
+     * @param date
+     */
 
     public MyDate(String date) {
         String[] temp = date.split("-");
@@ -18,29 +35,65 @@ public class MyDate {
         this.day = Integer.parseInt(temp[2]);
     }
 
+    /**
+     * gets the day of the start of end day of the project
+     * @return day
+     */
+
     public int getDay() {
         return day;
     }
+
+    /**
+     *sets the start or end day of a project
+     * @param day
+     */
 
     public void setDay(int day) {
         this.day = day;
     }
 
+    /**
+     * gets the start on end month of a project
+     * @return month
+     */
+
     public int getMonth() {
         return month;
     }
+
+    /**
+     * sets the start or end month of a project
+     * @param month
+     */
 
     public void setMonth(int month) {
         this.month = month;
     }
 
+    /**
+     * gets the start or end year of a project
+     * @return year
+     */
+
     public int getYear() {
         return year;
     }
 
+    /**
+     * sets the start or end year of a project
+     * @param year
+     */
+
     public void setYear(int year) {
         this.year = year;
     }
+
+    /**
+     * calculates the end date by adding the expected amount of time needed to finalize the project
+     * @param months
+     * @return end date
+     */
 
     public MyDate endDate(int months) {
         MyDate temp = this;
@@ -56,6 +109,12 @@ public class MyDate {
         }
         return temp;
     }
+
+    /**
+     * Checks if the given date is the same as the MyDate date
+     * @param obj
+     * @return true if the given date is the same as the MyDate date and false if they are different
+     */
     public boolean equals(Object obj)
     {
         if (obj== null || obj.getClass()!=this.getClass())
