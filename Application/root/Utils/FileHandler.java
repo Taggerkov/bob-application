936b9cc7f721fa9164fb138a4ff698888218bbec
BattyAlex <Alexandriaahansen@gmail.com>
2023-12-06 12:42:54 +0100

    Update FileHandler.java

--- Application/root/Utils/FileHandler.java
+++ Application/root/Utils/FileHandler.java
@@ -4,18 +4,43 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+/**
+ * Class for creating files and binary files
+ * @author Alexandria Hansen
+ * @version 1.0
+ */
 public class FileHandler
 {
+  /**
+   * Converts a string into text in a file with a given name
+   * @param fileName  the name and path of the file
+   * @param string    String to be converted to a text file
+   * @throws FileNotFoundException exception if a file with the name and path given in FileName doesn't exist
+   */
     public static void convertToTextFile(String fileName, String string) throws
         FileNotFoundException
     {
       write(fileName, string, false);
     }
+
+  /**
+   * Appends a string to a text file of a given name
+   * @param fileName    the name and path of the file
+   * @param string      String to be appended to the text file
+   * @throws FileNotFoundException exception if a file with the name and path given in FileName doesn't exist
+   */
     public static void appendToTextFile(String fileName, String string) throws FileNotFoundException
     {
       write(fileName, string, true);
     }
 
+  /**
+   * Method to convert a string to text file, used by convertToTextFile and appendToTextFile
+   * @param fileName  the name and path of the file
+   * @param string    String to be appended to the text file
+   * @param append    boolean for whether the text should be appended or not
+   * @throws FileNotFoundException exception if a file with the name and path given in FileName doesn't exist
+   */
     private static void write(String fileName, String string, boolean append) throws FileNotFoundException
     {
       PrintWriter convertToFile = null;
@@ -35,17 +60,33 @@ public class FileHandler
       }
     }
 
-
+  /**
+   *    Converts strings from array into text in a file with a given name
+   *    @param fileName  the name and path of the file
+   *    @param strings    String array to be converted to a text file
+   *    @throws FileNotFoundException exception if a file with the name and path given in FileName doesn't exist
+   */
     public static void convertArrayToTextFile(String fileName, String[] strings) throws FileNotFoundException
     {
       writeText(fileName, strings, false);
     }
-
+  /**
+   * Appends strings from an array to a text file of a given name
+   * @param fileName    the name and path of the file
+   * @param strings      String array to be appended to the text file
+   * @throws FileNotFoundException exception if a file with the name and path given in FileName doesn't exist
+   */
     public static void appendArrayToTextFile(String fileName, String[] strings) throws FileNotFoundException
     {
       writeText(fileName, strings, true);
     }
-
+  /**
+   * Method to convert strings from an array to text file, used by convertArrayToTextFile and appendArrayToTextFile
+   * @param fileName  the name and path of the file
+   * @param strings   String array to be appended to the text file
+   * @param append    boolean for whether the text should be appended or not
+   * @throws FileNotFoundException exception if a file with the name and path given in FileName doesn't exist
+   */
     private static void writeText(String fileName, String[] strings, boolean append) throws FileNotFoundException
     {
       PrintWriter convertToFile = null;
@@ -69,6 +110,12 @@ public class FileHandler
       }
     }
 
+  /**
+   * 
+   * @param fileName
+   * @return
+   * @throws FileNotFoundException
+   */
     public String readTextFile(String fileName) throws FileNotFoundException
     {
       Scanner readFile = null;
