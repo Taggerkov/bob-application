b6ddabe93b28e609da47e59a49775cb514496c8f
BattyAlex <Alexandriaahansen@gmail.com>
2023-12-06 09:27:49 +0100

    ModelManager start

--- Application/root/GUI/GUIController.java
+++ Application/root/GUI/GUIController.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import Model.ModelManager;
 import javafx.fxml.FXML;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
--- Application/root/Model/ModelManager.java
+++ Application/root/Model/ModelManager.java
@@ -2,8 +2,38 @@ package Model;
 
 import Utils.FileHandler;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class ModelManager
 {
-  private FileHandler fileHander;
+  private String fileName;
+
+  public ModelManager(String fileName)
+  {
+    this.fileName = fileName;
+  }
+
+  public ProjectList getAllProjects()
+  {
+    ProjectList allProjects = new ProjectList();
+    try
+    {
+      allProjects = (ProjectList) FileHandler.getFromBinaryFile(fileName);
+    }
+    catch (FileNotFoundException e)
+    {
+      System.out.println("File not found");
+    }
+    catch (IOException e)
+    {
+      System.out.println("IO Error reading file");
+    }
+    catch (ClassNotFoundException e)
+    {
+      System.out.println("Class Not Found");
+    }
+    return allProjects;
+  }
 
 }
