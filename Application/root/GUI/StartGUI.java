387776e30187e72522bbce303f584066c054009b
BattyAlex <Alexandriaahansen@gmail.com>
2023-12-13 09:41:19 +0100

    Update StartGUI.java

--- Application/root/GUI/StartGUI.java
+++ Application/root/GUI/StartGUI.java
@@ -4,29 +4,36 @@ import Model.ProjectManager;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.*;
 
 public class StartGUI extends Application {
 
-    public void start(Stage window)
-    {
-        try{
+    public void start(Stage window) {
+        try {
+            // Check if the directory exists, create it if not
+            File savesDirectory = new File("Saves");
+            if (!savesDirectory.exists()) {
+                savesDirectory.mkdir();
+            }
+
+            // Now, try to open the file
             FileInputStream fileIn = new FileInputStream("Saves/projects.bin");
-            ObjectInputStream read = new ObjectInputStream(fileIn);
+            fileIn.close();  // Close the input stream
             System.out.println(".bin file detected.");
-        } catch (Exception e){
-            try{
+        } catch (FileNotFoundException e) {
+            try {
+                // If the file doesn't exist, create it
                 FileOutputStream fileOut = new FileOutputStream("Saves/projects.bin");
-                ObjectOutputStream write = new ObjectOutputStream(fileOut);
+                fileOut.close();  // Close the output stream
                 System.out.println(".bin file created.");
-            } catch (Exception E){
+            } catch (IOException ex) {
                 System.out.println("Unable to I/O!");
-                E.printStackTrace();
+                ex.printStackTrace();
                 System.exit(0);
             }
+        } catch (IOException e) {
+            // Handle other IOExceptions if needed
+            e.printStackTrace();
         }
 
         ProjectManager manager = new ProjectManager("Saves/projects.bin");
